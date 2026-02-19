eedafdb7adbff9be188aebfc321fc920de329116
Ryan <ryanleibbrand1@gmail.com>
2026-02-19 09:30:30 +0100

    Display random character image from dataset

--- Php/Wie-is-het/student_assignments/assignment6.php
+++ Php/Wie-is-het/student_assignments/assignment6.php
@@ -2,5 +2,12 @@
     require_once __DIR__ . '/../includes/load_data.php';
     $characterDataset = load_data();
 
+    $rad = array_rand($characterDataset);
+
+        $img = "../images/{$rad}.png";
+        echo $rad. "<br>". "<img alt='s' src=$img ><br>";
+
+
+
     // Opdracht 6: Kies een willekeurig personage en toon zijn/haar afbeelding.
     // Tip: Gebruik de array_rand() functie om een willekeurige key uit de array te halen.
