25e767bc3f9a23b94401b2d1d90e076f2969bcda
Ryan <ryanleibbrand1@gmail.com>
2026-03-06 12:17:28 +0100

    text changes when pressing button
    Added functionality to switch between API and database search modes, updating placeholders and current selection accordingly. ....weird ai i just added text changing on button press

--- JS/eindopdracht.js
+++ JS/eindopdracht.js
@@ -1,13 +1,43 @@
 window.addEventListener("load", function () {
+
+    if (!localStorage.getItem("selected")) {
+        localStorage.setItem("selected", 'Api');
+    }
+
+    if (localStorage.getItem("selected") === "DataBase"){
+        document.querySelector('#fsearch').placeholder = "Search for a Pokemon..."
+    }
+
+    document.querySelector('#current').textContent = `Current: ${localStorage.getItem("selected")}`;
+
     document.querySelector('#fbutton').addEventListener('click', async function () {
+        event.preventDefault()
+
         let query = document.querySelector('#fsearch').value
+        let type = localStorage.getItem("selected")
 
         const urlParams = new URLSearchParams(window.location.search);
 
         urlParams.set('query', query);
+        urlParams.set('type', type);
 
         window.location.search = urlParams;
         //window.location.reload()
+    });
+
+    document.querySelector('#btnApi').addEventListener('click', async function() {
+        localStorage.setItem("selected", "Api");
+        document.querySelector('#current').textContent = `Current: ${localStorage.getItem("selected")}`;
+        document.querySelector('#fsearch').placeholder = "Search for a Country..."
+        let content = (getComputedStyle(document.querySelector('form'), ':before').getPropertyValue('content'));
+        content.replaceAll("Pokemon Search", "Country Search")
+    });
+    document.querySelector('#btnDataBase').addEventListener('click', async function() {
+        localStorage.setItem("selected", "DataBase");
+        document.querySelector('#current').textContent = `Current: ${localStorage.getItem("selected")}`;
+        document.querySelector('#fsearch').placeholder = "Search for a Pokemon..."
+        let content = (getComputedStyle(document.querySelector('form'), ':before').content);
+        content.replaceAll("Country Search", "Pokemon Search")
     });
 });
 
