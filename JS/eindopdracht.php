84b3ca371ace0978f6e599c1b8c73be1d6689265
Ryan <ryanleibbrand1@gmail.com>
2026-03-06 12:17:53 +0100

    Refactor country search logic and UI elements

--- JS/eindopdracht.php
+++ JS/eindopdracht.php
@@ -6,40 +6,44 @@ function fetchCountry($url) {
 }
 
 $query = $_GET['query'] ?? null;
+$error = "";
+$countryResult = "";
 
 if (!$query) {
-    die("Error: `query` parameter must be present.");
+    $countryResult = "<p>Error: `query` parameter must be present.</p>";
 }
 
+if ($query) {
 $query = strtolower($query);
 
-$data = fetchCountry("https://restcountries.com/v3.1/name/$query");
+if ($_GET['type'] === 'Api') {
+    $data = fetchCountry("https://restcountries.com/v3.1/name/$query");
 
-if (!$data) {
-    $data = fetchCountry("https://restcountries.com/v3.1/name/$query?fullText=true");
-}
+    if (!$data) {
+        $data = fetchCountry("https://restcountries.com/v3.1/name/$query?fullText=true");
+    }
 
-if (!$data) {
-    $data = fetchCountry("https://restcountries.com/v3.1/capital/$query");
-}
+    if (!$data) {
+        $data = fetchCountry("https://restcountries.com/v3.1/capital/$query");
+    }
 
-if (!$data) {
-    die("No matching country found.");
-}
+    if (!$data) {
+        $countryResult = "<p>No matching country found.</p>";
+    }
 
-$country = $data[0];
+    if ($data) {
+        $country = $data[0];
+        $name = $country["name"]["common"];
+        $region = $country["region"];
+        $subRegion = $country["subregion"];
+        $capital = $country["capital"][0] ?? "Unknown";
+        $population = $country["population"] ?? "Unknown";
+        $languages = implode(", ", $country["languages"]);
+        $currencyCode = array_key_first($country["currencies"]);
+        $currencyName = $country["currencies"][$currencyCode]["name"];
+        $flag = $country["flags"]["png"];
 
-$name = $country["name"]["common"];
-$region = $country["region"];
-$subRegion = $country["subregion"];
-$capital = $country["capital"][0] ?? "Unknown";
-$population = $country["population"] ?? "Unknown";
-$languages = implode(", ", $country["languages"]);
-$currencyCode = array_key_first($country["currencies"]);
-$currencyName = $country["currencies"][$currencyCode]["name"];
-$flag = $country["flags"]["png"];
-
-$countryResult = "
+        $countryResult = "
             <div class='country-result'>
                 <p><strong>Name:</strong> $name</p>
                 <p><strong>Region:</strong> $region</p>  
@@ -51,6 +55,76 @@ $countryResult = "
                 <img src='$flag' alt='$flag'>
             </div>
         ";
+    }
+}
+}
+
+if ($_GET['type'] === 'DataBase')
+{
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "Tweedinosdiedansen2!";
+    $dbname = "pokemon";
+
+// Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if (is_numeric($query)) {
+        $sql = "
+        select * from pokemon_types join types
+        on pokemon_types.type_id = types.id
+        join pokemon on pokemon_types.pokemon_id = pokemon.id
+        where pokemon.id = '$query'";
+    }
+
+    if (!is_numeric($query)) {
+        $sql = "
+        select * from pokemon_types join types
+        on pokemon_types.type_id = types.id
+        join pokemon on pokemon_types.pokemon_id = pokemon.id
+        where pokemon.name = '$query'";
+    }
+    $result = $conn->query($sql);
+    $hit = 0;
+    $type = null;
+
+    if ($result->num_rows > 0) {
+        // Output data of each row
+        while($row = $result->fetch_assoc()) {
+            if ($hit == 1){
+                $type = $type."/".ucfirst($row["t"]);
+            }
+            if ($hit == 0) {
+                $hit = 1;
+                $id = $row["id"];
+                $name = ucfirst($row["name"]);
+                if (!$type) {$type = ucfirst($row["t"]);}
+                $height = $row["height"];
+                $weight = $row["weight"];
+            }
+        }
+        $countryResult = "
+            <div class='country-result'>
+                <p><strong>Id:</strong> $id</p>
+                <p><strong>Name:</strong> $name</p>  
+                <p><strong>Type:</strong> $type</p>
+                <p><strong>Height:</strong> $height</p>
+                <p><strong>Weight:</strong> $weight</p>
+                <img src='Images/Pokemon/$id.png' alt='a'>
+            </div>
+        ";
+    }
+    else {
+        $countryResult = "<div>0 results</div>";
+    }
+
+    $conn->close();
+}
 ?>
 
 <!DOCTYPE html>
@@ -63,12 +137,17 @@ $countryResult = "
 <body>
 
 <script src="eindopdracht.js"></script>
-
+<dl>
+    <dt><button id="btnApi">Api</button></dt>
+    <dt><button id="btnDataBase">DataBase</button></dt>
+    <dt><h1 id="current">Current: </h1></dt>
+</dl>
 <form method="GET">
-    <input type="search" id="fsearch" name="query" placeholder="Search for a country...">
+    <input type="search" id="fsearch" name="query" placeholder="Search for a Country...">
     <input type="submit" id="fbutton" value="Search">
 </form>
 
+<?= $error ?>
 <?= $countryResult ?>
 
 </body>
